a6db5ce57ae0db25b584219e91b3b98294411b59
barruumrex <barruumrex@gmail.com>
2016-06-12 16:48:03 -0400

    Working for fixnums

--- lib/marshal.ex
+++ lib/marshal.ex
@@ -1,2 +1,20 @@
 defmodule Marshal do
+
+  def decode(<<major::size(8), minor::size(8), rest::binary>>), do: {"#{major}.#{minor}", decode_element(rest)}
+
+  defp decode_element(<<"0">>), do: nil
+  defp decode_element(<<"T">>), do: true
+  defp decode_element(<<"F">>), do: false
+  defp decode_element(<<"i", rest::binary>>), do: decode_fixnum(rest)
+  defp decode_element(rest), do: rest
+
+  defp decode_fixnum(<<0>>), do: 0
+  defp decode_fixnum(<<num::signed-little-integer, rest::binary>>) when abs(num) < 5, do: decode_multibyte_fixnum(abs(num), rest)
+  defp decode_fixnum(<<num::signed-little-integer>>) when num > 0, do: num - 5
+  defp decode_fixnum(<<num::signed-little-integer>>) when num < 0, do: num + 5
+
+  defp decode_multibyte_fixnum(4, <<num::signed-little-integer-size(32)>>), do: num
+  defp decode_multibyte_fixnum(3, <<num::signed-little-integer-size(24)>>), do: num
+  defp decode_multibyte_fixnum(2, <<num::signed-little-integer-size(16)>>), do: num
+  defp decode_multibyte_fixnum(1, <<num::signed-little-integer-size(8)>>), do: num
 end
--- test/marshal_test.exs
+++ test/marshal_test.exs
@@ -2,7 +2,43 @@ defmodule MarshalTest do
   use ExUnit.Case
   doctest Marshal
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  test "Decode nil" do
+    assert Marshal.decode("\x04\b0") == {"4.8", nil}
+  end
+
+  test "Decode True" do
+    assert Marshal.decode("\x04\bT") == {"4.8", true}
+  end
+
+  test "Decode False" do
+    assert Marshal.decode("\x04\bF") == {"4.8", false}
+  end
+
+  test "Decode 0" do
+    assert Marshal.decode("\x04\bi\x00") == {"4.8", 0}
+  end
+
+  test "Decode 1" do
+    assert Marshal.decode("\x04\bi\x06") == {"4.8", 1}
+  end
+
+  test "Decode -1" do
+    assert Marshal.decode("\x04\bi\xFA") == {"4.8", -1}
+  end
+
+  test "Decode 500" do
+    assert Marshal.decode("\x04\bi\x02\xF4\x01") == {"4.8", 500}
+  end
+
+  test "Decode -500" do
+    assert Marshal.decode("\x04\bi\xFE\f\xFE") == {"4.8", -500}
+  end
+
+  test "Decode max fixnum" do
+    assert Marshal.decode("\x04\bi\x04\xFF\xFF\xFF?") == {"4.8", 1073741823}
+  end
+
+  test "Decode min fixnum" do
+    assert Marshal.decode("\x04\bi\xFC\x00\x00\x00\xC0") == {"4.8", -1073741824}
   end
 end
