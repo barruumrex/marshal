f5b3acb69d07a3c76eeb5bacbaf4e2f34df3ffb5
barruumrex <barruumrex@gmail.com>
2016-06-21 22:56:39 -0400

    Decode time

--- lib/usrdef/time.ex
+++ lib/usrdef/time.ex
@@ -10,7 +10,7 @@ defimpl Marshal.UsrDef, for: UsrDef.Time do
     {size, rest} = Marshal.Decode.Helper.decode_fixnum(bits)
     <<number::binary-size(size), rest::binary>> = rest
 
-    usrdef = {:usrdef, :Time, number}
+    usrdef = {:usrdef, :Time, decode_time(number)}
 
     # Manually get_tuples
     {vars, rest, cache} = Marshal.Decode.Helper.get_tuples(rest, cache)
@@ -19,6 +19,23 @@ defimpl Marshal.UsrDef, for: UsrDef.Time do
     rest = <<0>> <> rest
 
     {{usrdef, vars}, rest, cache}
+  end
+
+  defp decode_time(bits) do
+    bits
+    |> fix_order()
+    |> unpack()
+  end
+
+  defp fix_order(<<p4, p3, p2, p1, s4, s3, s2, s1>>), do: <<p1, p2, p3, p4, s1, s2, s3, s4>>
+
+  defp unpack(<<1::1, utc_p::1, year::16, month::4, day::5, hour::5, min::6, sec::6, usec::20>>) do
+    year = year + 1900
+    month = month + 1
+
+    {:ok, date} = Date.new(year, month, day)
+    {:ok, time} = Time.new(hour, min, sec, usec)
+    {date, time}
   end
 
 end
--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Marshal.Mixfile do
   def project do
     [app: :marshal,
      version: "0.0.1",
-     elixir: "~> 1.3-rc",
+     elixir: "~> 1.3.0",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      deps: deps]
